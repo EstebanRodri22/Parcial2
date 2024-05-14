c4642de6cfd779e9d99bb5a8431e2a4f0d937727
CHEGUAN <CHEGUAN@DESKTOP-7INUJ9P>
2024-05-13 20:17:51 -0500

    factura modificada

--- GUI/Factura.Designer.cs
+++ GUI/Factura.Designer.cs
@@ -29,53 +29,86 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtReferencia = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.labNombre = new System.Windows.Forms.Label();
+            this.labValorUni = new System.Windows.Forms.Label();
+            this.labStock = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(60, 109);
+            this.label1.Location = new System.Drawing.Point(63, 79);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(54, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "referencia";
             // 
-            // textBox1
+            // txtReferencia
             // 
-            this.textBox1.Location = new System.Drawing.Point(184, 106);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 1;
+            this.txtReferencia.Location = new System.Drawing.Point(142, 76);
+            this.txtReferencia.Name = "txtReferencia";
+            this.txtReferencia.Size = new System.Drawing.Size(112, 20);
+            this.txtReferencia.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(249, 28);
+            this.label2.Location = new System.Drawing.Point(139, 25);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(84, 13);
             this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
+            this.label2.Text = "venta productos";
             // 
-            // button1
+            // btnBuscar
             // 
-            this.button1.Location = new System.Drawing.Point(137, 224);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnBuscar.Location = new System.Drawing.Point(142, 135);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 3;
+            this.btnBuscar.Text = "buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // labNombre
+            // 
+            this.labNombre.AutoSize = true;
+            this.labNombre.Location = new System.Drawing.Point(43, 210);
+            this.labNombre.Name = "labNombre";
+            this.labNombre.Size = new System.Drawing.Size(42, 13);
+            this.labNombre.TabIndex = 4;
+            this.labNombre.Text = "nombre";
+            // 
+            // labValorUni
+            // 
+            this.labValorUni.AutoSize = true;
+            this.labValorUni.Location = new System.Drawing.Point(150, 210);
+            this.labValorUni.Name = "labValorUni";
+            this.labValorUni.Size = new System.Drawing.Size(67, 13);
+            this.labValorUni.TabIndex = 5;
+            this.labValorUni.Text = "valor unitario";
+            // 
+            // labStock
+            // 
+            this.labStock.AutoSize = true;
+            this.labStock.Location = new System.Drawing.Point(280, 210);
+            this.labStock.Name = "labStock";
+            this.labStock.Size = new System.Drawing.Size(33, 13);
+            this.labStock.TabIndex = 6;
+            this.labStock.Text = "stock";
             // 
             // Factura
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(371, 383);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(371, 457);
+            this.Controls.Add(this.labStock);
+            this.Controls.Add(this.labValorUni);
+            this.Controls.Add(this.labNombre);
+            this.Controls.Add(this.btnBuscar);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtReferencia);
             this.Controls.Add(this.label1);
             this.Name = "Factura";
             this.Text = "Factura";
@@ -87,8 +120,11 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtReferencia;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Label labNombre;
+        private System.Windows.Forms.Label labValorUni;
+        private System.Windows.Forms.Label labStock;
     }
 }
